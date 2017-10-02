88fbd7c29832599bfa294e667fbc15e3a7aaf448
Alexandra Ingrando <aingrando@smu.edu>
2017-10-02 11:30:14 -0500

    update

--- ConditionalSuffixTree/src/MainTree.java
+++ ConditionalSuffixTree/src/MainTree.java
@@ -8,19 +8,43 @@ public class MainTree {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ArrayList<Character> data = new ArrayList<Character>();
-		data.addAll(Arrays.asList('a', 'b', 'r', 'a', 'c', 'a', 'd', 'a', 'b', 'r', 'a'));
 		
-		ArrayList<Character> data1 = new ArrayList<Character>();
-		data1.addAll(Arrays.asList('a', 'b', 'c', 'c', 'c', 'd', 'a', 'a', 'd', 'c', 'd', 'a', 'a', 'b', 'c', 'a', 'd'));
-		
-		unitTest(3, .1, 2, data);
-		unitTest(3, .1, 2, data1);
+		unitTestAbra(3, .1, 2);
+		unitTestPTS(3, .1, 2);
 		
 	}
 	
-	public static void unitTest(int l, double pMin, double r, ArrayList<Character> data) {
+	public static void unitTestAbra(int l, double pMin, double r) {
+		
+		ArrayList<Character> data = new ArrayList<Character>();
+		data.addAll(Arrays.asList('a', 'b', 'r', 'a', 'c', 'a', 'd', 'a', 'b', 'r', 'a'));
+		
+		//initialize the tree's for each data
+		Tree<Character> funTree = new Tree<Character>(l, pMin, r, data);
 
+		
+		//receives and reads data to determine patterns
+		funTree.train();
+		
+		//print every pattern found
+		//funTree.printAllPatterns();
+		
+		//print each branch and connections
+		//funTree.printFirstBranch();
+		
+		//prints the patterns that have an empirical probability greater than pMin
+		//funTree.printEmpirical();
+		
+		//prints the patterns that passed both empirical and conditional test
+		funTree.printConditional();
+
+	}
+	
+public static void unitTestPTS(int l, double pMin, double r) {
+		
+		ArrayList<Character> data = new ArrayList<Character>();
+		data.addAll(Arrays.asList('a', 'b', 'c', 'c', 'c', 'd', 'a', 'a', 'd', 'c', 'd', 'a', 'a', 'b', 'c', 'a', 'd'));
+		
 		//initialize the tree's for each data
 		Tree<Character> funTree = new Tree<Character>(l, pMin, r, data);
 
